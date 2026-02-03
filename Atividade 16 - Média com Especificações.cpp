20457fb2a708b20a373a38280053db9fe4b58739
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-03 20:11:20 -0300

    Update and rename Correção_9_22042025_Algoritmo.cpp to Atividade 16 - Média com Especificações.cpp

---
+++ Atividade 16 - Média com Especificações.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+ 
+ float p1,p2,media,atividade;
+
+ printf("Digite a nota da prova 1: ");
+ scanf ("%f",&p1);
+ printf("Digite a nota da prova 2: ");
+ scanf ("%f",&p2);
+ printf("Digite a nota das atividades do Semestre: ");
+ scanf ("%f",&atividade);
+
+ media=((p1*4)+(p2*4)+(atividade*2))/10; //Cálculo da média com regras de especificação do valor atribuido a cada variável
+
+ printf("A media final e: %3.2f",media);
+
+ return 0;
+}
+
+
