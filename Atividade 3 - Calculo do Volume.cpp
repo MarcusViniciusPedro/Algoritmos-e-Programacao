4e92563b0e796c1cb76373b94592a50eff7af9ad
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-03 19:40:28 -0300

    Add Atividade 3 - Calculo do Volume.cpp file

--- Atividade 3 - Calculo do Volume.cpp
+++ Atividade 3 - Calculo do Volume.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+//Esse algoritmo coleta duas variáveis float para fazer o cálculo do volume e apresentar no final do programa seu resultado
 int main(){
 	
 	float R, A, V;
@@ -10,9 +10,10 @@ int main(){
 	printf("Entre com o valor da altura: ");
 	scanf("%f", &A);
 	
-	V = M_PI * R * R * A;
+	V = M_PI * R * R * A; //Calculo do Volume
 	
 	printf("O valor do volume e:\n %.2f", V);
 	
 	return 0;
 }
+
