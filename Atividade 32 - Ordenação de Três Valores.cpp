65eee79f420dd5b04fac203c96d3b73549d9765e
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-04 12:55:26 -0300

    Update and rename Atividade_8_25032025_Algoritmo.cpp to Atividade 32 - Ordenação de Três Valores.cpp

--- Atividade 32 - Ordenação de Três Valores.cpp
+++ Atividade 32 - Ordenação de Três Valores.cpp
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void ordenar_tres_valores(int *a, int *b, int *c){
+void ordenar_tres_valores(int *a, int *b, int *c){ //A void será chamada pela função main, essa função é um princípio de Bubble Sort
     int temp;
 
-    if (*a > *b) {
+    if (*a > *b) { //Se a for maior que b
         temp = *a;
         *a = *b;
         *b = temp;
     }
-    if (*a > *c) {
+    if (*a > *c) { //Se a maior que c
         temp = *a;
         *a = *c;
         *c = temp;
     }
 
-    if (*b > *c) {
+    if (*b > *c) { //Se b maior que c
         temp = *b;
         *b = *c;
         *c = temp;
@@ -31,9 +31,10 @@ int main(){
     printf("Digite numero: ");
     scanf("%d", &num3);
 
-    ordenar_tres_valores(&num1, &num2, &num3);
+    ordenar_tres_valores(&num1, &num2, &num3); //Função void é chamada nesse instante após os valores serem coletados pelos scanf's
 
     printf("Numeros ordenados em ordem crescente: %d %d %d", num1, num2, num3);
 
     return 0;
 }
+
