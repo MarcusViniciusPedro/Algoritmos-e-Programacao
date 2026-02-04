c2074ba669fcf09af6023c9173300f99a4fcd379
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-04 19:55:29 -0300

    Update and rename Atividade_3_14042025_Algoritmo.cpp to Atividade 38 - Sequência de Par e Ímpar.cpp

--- Atividade 38 - Sequência de Par e Ímpar.cpp
+++ Atividade 38 - Sequência de Par e Ímpar.cpp
@@ -6,14 +6,15 @@ int main (){
 	
 	while (cont <= 20){
 		
-		if (cont %2 == 0){
+		if (cont %2 == 0){ //Quando divisível por 2 será apresentado como par
 		printf("%.2i - par\n", cont);
 		cont = cont + 1;
 	    }
-		else {
+		else { //Qualquer outro valor será apresentado como ímpar
 		printf("%.2i - impar\n", cont);
 		cont = cont + 1;
 	    }
 	}
 	return 0;
 }
+
