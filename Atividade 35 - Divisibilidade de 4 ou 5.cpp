2a7babf447dacac0b64b7cbd94a38d8bd914edef
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-04 13:00:59 -0300

    Add divisibility check for 4 or 5 in C program

--- Atividade 35 - Divisibilidade de 4 ou 5.cpp
+++ Atividade 35 - Divisibilidade de 4 ou 5.cpp
@@ -8,14 +8,15 @@ int main (){
 	scanf("%i %i", &N1, &N2);
 	printf("Os numeros divisiveis por 4 ou 5 sao:\n");
 	
-    if(N1 % 4 == 0 || N1 % 5 == 0)
+    if(N1 % 4 == 0 || N1 % 5 == 0) //N1 divisível por 4 ou N1 divisível por 5
 	   printf("%i\n", N1);
-	if(N2 % 4 == 0 || N2 % 5 == 0)
+	if(N2 % 4 == 0 || N2 % 5 == 0) //N2 divisível por 4 ou N2 divisível por 5
 	   printf("%i\n", N2);
-	else
+	else //Qualquer outra alternativa será considerada como um valor não divisível por eles
 	   printf("\nNenhum dos dois numeros e divisivel por 4 ou 5.\n");
 	   
 	return 0;
 }
+
 
 
