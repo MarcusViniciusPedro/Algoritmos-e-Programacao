79e36cbd6ef2c29d71fe3b43d02c498fc39ef33d
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-04 12:39:39 -0300

    Enhance code clarity with comments
    Added comments to clarify the logic for value subtraction based on comparison.

--- Atividade 29 - Diferença entre dois Valores.cpp
+++ Atividade 29 - Diferença entre dois Valores.cpp
@@ -9,14 +9,15 @@ int main(){
 	printf("Valor de V2: ");
 	scanf("%f", &V2);
 	
-	if(V1>V2){ 
-		Va = V1 - V2;
+	if(V1>V2){ //Se V1 for maior que V2
+		Va = V1 - V2; //A subtração será V1 - V2 para que o resultado não seja negativo
 		printf("Valor: %.2f", Va);
 	}
-	    if(V1<V2){
-	        Vo = V2 - V1; 
+	    if(V1<V2){ //Se V1 for menor que V2
+	        Vo = V2 - V1; //A subtração se inverte, para que, novamente não dê negativo
 	        printf("Valor: %.2f", Vo);
         }
 	   
 	return 0;
 }
+
