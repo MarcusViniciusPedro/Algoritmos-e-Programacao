a97030981357716cec560f60525f3a2d85bae2a6
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-04 12:58:55 -0300

    Update and rename Atividade_8.1_01042025_Algoritmo.cpp to Atividade 34 - Divisibilidade por 2 e 3.cpp

--- Atividade 34 - Divisibilidade por 2 e 3.cpp
+++ Atividade 34 - Divisibilidade por 2 e 3.cpp
@@ -8,14 +8,15 @@ int main (){
 	scanf("%i %i %i", &N1, &N2, &N3);
 	printf("Os numeros divisiveis por 2 e 3 sao:\n");
 	
-	if (N1 % 6 == 0)
+	if (N1 % 6 == 0) //Se N1 ao ser dividido por 6 restará 0
 	   printf("%i\n", N1);
 	
-	if (N2 % 6 == 0)
+	else if (N2 % 6 == 0) //Se N2 ao ser dividido por 6 restará 0
 	   printf("%i\n", N2);
 	   
-	else (N3 % 6 == 0)
+	else if (N3 % 6 == 0) //Se N3 ao ser dividido por 6 restará 0
 	   printf("%i\n", N3);
 
 	return 0;
 }
+
