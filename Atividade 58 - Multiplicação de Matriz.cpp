ce83acc954e4f75401a359817350e3b66bea8fac
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 19:16:41 -0300

    Update and rename Atividade_1_15042025_Algoritmo.cpp to Atividade 58 - Multiplicação de Matriz.cpp

--- Atividade 58 - Multiplicação de Matriz.cpp
+++ Atividade 58 - Multiplicação de Matriz.cpp
@@ -7,14 +7,15 @@ int main () {
 	
 	printf("Entre com os numeros(5) do vetor:\n");
 	
-	for(I = 0; I <= 4; ++I){
+	for(I = 0; I <= 4; ++I){ //Entra com 5 valores no for
 		printf("Numero %i: ", I + 1);
 		scanf("%i", &a[I]);
 	}
 	printf("A matriz b e:\n");
-	for(I = 0; I <= 4; ++I){
+	for(I = 0; I <= 4; ++I){ // A matriz multiplica cada número no vetor por 3
 	b[I] = a[I] * 3;
 	printf("Numero %i: %i\n", I + 1, b[I]);
     }
 	return 0;
 }
+
