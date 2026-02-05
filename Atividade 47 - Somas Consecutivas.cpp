b3fd47c83b406e593852ad5f7057756e55004fcd
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 16:28:31 -0300

    Update and rename Atividade_2_01042025_Algoritmo.cpp to Atividade 47 - Somas Consecutivas.cpp

--- Atividade 47 - Somas Consecutivas.cpp
+++ Atividade 47 - Somas Consecutivas.cpp
@@ -5,10 +5,11 @@ int main (){
 	
 	int X, soma = 0;
 	
-	for (X = 1; X <= 20; ++X){
+	for (X = 1; X <= 20; ++X){ //Serão somados os 20 primeiros valores
 		
-		soma = soma + X;
+		soma = soma + X; //O valor de X será somado com a soma 20 vezes
 		printf("\nA soma e:%i\n", soma);
 	}
 	return 0;
 }
+
