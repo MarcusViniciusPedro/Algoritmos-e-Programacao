897efa419309a7c02918cf8f06b7682496584a6b
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 16:41:55 -0300

    Update and rename Atividade_5_08042025_Algoritmo.cpp to Atividade 49 - Números Divisíveis por 4.cpp

--- Atividade 49 - Números Divisíveis por 4.cpp
+++ Atividade 49 - Números Divisíveis por 4.cpp
@@ -4,7 +4,7 @@ int main(){
 	
 	int cont; {
 	
-	for (cont = 1; cont <= 200; cont = cont + 1)
+	for (cont = 1; cont <= 200; cont = cont + 1) //Fará a contagem de quantos números são divisíveis por 4
 	if (cont % 4 == 0)
 	printf ("\ndivisiveis por 4: %d", cont);
 	}
@@ -12,3 +12,4 @@ int main(){
 	return 0;
 		
 }
+
